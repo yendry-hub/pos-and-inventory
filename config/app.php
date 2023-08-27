a4c8e0aed8bb8b8ca32916ec9df0c8233290be3f
yendry-hub <64301229+yendry-hub@users.noreply.github.com>
2023-08-27 16:33:58 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -81,7 +81,7 @@ return [
      */
 
     'locale' => 'en',
-
+    'number_format' => ',.',
     /*
     |--------------------------------------------------------------------------
     | Application Fallback Locale
